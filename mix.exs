eaddefcb254cc9ff63238edb3a9a866b11984ecb
José Eduardo <josee.edumont@gmail.com>
2020-08-07 22:17:27 -0300

    Added Jason to decode JSON responses

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule GithubCli.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.7"}
+      {:httpoison, "~> 1.7"},
+      {:jason, "~> 1.2"}
     ]
   end
 
