d5678ae1f7d624aa1dfd0268906c6050829457b0
José Eduardo <josee.edumont@gmail.com>
2020-08-26 17:09:24 -0300

    Changed base_url from users to repos

--- lib/github_cli/commands/repos_commands.ex
+++ lib/github_cli/commands/repos_commands.ex
@@ -1,6 +1,6 @@
 defmodule GithubCli.Commands.ReposCommands do
   use Tesla
 
-  plug(Tesla.Middleware.BaseUrl, "https://api.github.com/users/")
+  plug(Tesla.Middleware.BaseUrl, "https://api.github.com/repos/")
   plug(Tesla.Middleware.JSON)
 end
