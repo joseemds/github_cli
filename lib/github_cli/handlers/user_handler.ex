ddf006239accda5a5d7da2be518c72a14e4343e9
José Eduardo <josee.edumont@gmail.com>
2020-08-26 16:52:30 -0300

    Implemented show user to take only the necessaries fields from the map

--- lib/github_cli/handlers/user_handler.ex
+++ lib/github_cli/handlers/user_handler.ex
@@ -1,7 +1,11 @@
 defmodule GithubCli.Handler.UserHandler do
   alias GithubCli.Helpers.DisplayHelper
 
+  @user_attrs ~w"name email repos_url url followers"
+
   def show_user({:ok, %{body: body}}) do
-    DisplayHelper.to_table(body)
+    body
+    |> Map.take(@user_attrs)
+    |> DisplayHelper.to_table()
   end
 end
