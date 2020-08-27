c1a83c2fb20ba036b61dc23773c82b198c305303
José Eduardo <josee.edumont@gmail.com>
2020-08-26 21:01:38 -0300

    Printing info from request of repository issues

--- lib/github_cli/handlers/repo_handler.ex
+++ lib/github_cli/handlers/repo_handler.ex
@@ -2,4 +2,8 @@ defmodule GithubCli.Handler.RepoHandler do
   def show_repo({:ok, %{body: body}}) do
     IO.inspect(body)
   end
+
+  def show_repo_issues({:ok, %{body: body}}) do
+    IO.inspect(body)
+  end
 end
